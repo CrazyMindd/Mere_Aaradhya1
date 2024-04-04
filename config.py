afa9ab4fc6191cdd8f4cfdca6089d5b79669cf48
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-04-04 09:53:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 29707621
+
+API_HASH = os.environ.get("API_HASH", "a1f31eeb31a0b9e36f551da6b8b85752")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6756963411:AAHAxM3x5PlSXMLXp3hZyKp_Ih9D_30MfXY")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6707073614))
+
+LOG = -1002131532222
+
